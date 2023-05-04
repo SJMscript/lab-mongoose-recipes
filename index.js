0e1eb22fef447e8c5daf7d55d5460a5f1752a43b
Santiago Massa <sjm_95@hotmail.com>
2023-05-04 17:43:22 +0200

    receta push

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,6 +17,24 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+     return Recipe.create({
+      title: "Pollo al ajillo",
+      level: "Easy Peasy",
+      ingredients: ["pollo", "ajo", "pimienta", "sal", "patatas", "caldo"],
+      cuisine: "Mediterranean",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 60,
+      creator: "Chef PolliMaster"
+    })
+  })
+  .then((recipeAdded) =>{
+    console.log("Recipe Title", recipeAdded.title)
+  })
+  .then(()=>{
+    return Recipe.insertMany({
+      
+    })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
